6191a7aca0fcaff8110b90f019916e0cab842bb9
Romanandhan <romanandhan@gmail.com>
2021-07-28 22:20:10 +0530

    Add sample turtle program

---
+++ first_turtle.py
@@ -0,0 +1,14 @@
+import turtle
+wn = turtle.getscreen()
+roma = turtle.Turtle()
+roma.forward(90)
+roma.left(90)
+roma.forward(90)
+roma.left(90)
+roma.forward(90)
+roma.left(90)
+roma.forward(90)
+roma.left(90)
+roma.backward(180)
+
+wn.exitonclick()
