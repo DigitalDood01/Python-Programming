624185614cb475626a09cbd02ccf71fe6adf06bf
Romanandhan <romanandhan@gmail.com>
2021-07-29 21:41:09 +0530

    Create program to understand strings, lists and tuples and its indexing

---
+++ lists,strings,tuples.py
@@ -0,0 +1,27 @@
+# Strings
+print("Let's see strings")
+s = """hello
+        dude
+        how are 
+        you!"""
+print(s)
+print(len(s))
+for i in range(48):
+    print(s[i])
+
+#Lists
+print("Let's see Lists")
+list = ['one', 1, 1.0]
+print(list)
+print(len(list))
+for i in range(3):
+    print(list[i])
+
+#Tuple
+print("Let's see Tuples")
+tuple = ('one', 1,1.0)
+print(tuple)
+print(len(tuple))
+for i in range(3):
+    print(tuple[i])
+
