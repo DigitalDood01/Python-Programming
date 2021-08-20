23a49d7527795f574d2864613ae862f48ef15b8e
Romanandhan <romanandhan@gmail.com>
2021-08-20 23:29:15 +0530

    create file to exploe the opeations in dictionaries

---
+++ Dictionary_operations.py
@@ -0,0 +1,9 @@
+# This Program is about exploring the options in Dictionary
+medals = {'gold':1, 'silver':2, 'bronze':3}
+print(medals)
+
+medals['gold'] = 23;
+print(medals)
+
+del medals['bronze']
+print(medals)
