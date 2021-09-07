f1af1da862101a87081800946d6238bdcde631f6
Romanandhan <romanandhan@gmail.com>
2021-09-07 22:12:06 +0530

    Create file to understand operations in dictionary

--- Dictionary_operations.py
+++ Dictionary_operations.py
@@ -7,3 +7,36 @@ print(medals)
 
 del medals['bronze']
 print(medals)
+
+data = len(medals)
+print(data)
+
+#Accessing the keys in a dictionary
+for key in medals.keys():
+    print(key)
+
+#Accessing both the keys and its values in a dictionary
+for key in medals.keys():
+    print(key, "and its value is", medals[key])
+
+#Accessing the the keys in dictionary as a list
+keys =list(medals.keys())
+print(keys)
+
+#Accessing the values in a dictionary
+for value in medals.values():
+    print(value)
+
+#Accessing the the keys and values on the whole in dictionary as a list
+keys =list(medals.items())
+print(keys)
+
+for key in medals:
+    print("got the key-", key)
+
+# Checking whether the key is present or not in a dictionary
+print('gold' in medals)
+print('bronze' in medals)
+
+print(medals.get('gold'))
+print(medals.get('diamond',0))
