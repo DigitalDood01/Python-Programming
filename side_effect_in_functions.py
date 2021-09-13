c7e85f76154bcceb3b3b58b94c74f82e357ab426
Romanandhan <romanandhan@gmail.com>
2021-09-13 19:00:28 +0530

    Create python file to study the difference between scope of variables and objects

---
+++ side_effect_in_functions.py
@@ -0,0 +1,13 @@
+def double(y):
+    y = 2 * y
+
+def changelist(lst):
+    lst[0] = 'I'
+    lst[1] = 'am'
+y = 5
+double(y)
+print(y)
+lst = ['we', 'are', 'learning', 'python']
+print(lst)
+changelist(lst)
+print(lst)
