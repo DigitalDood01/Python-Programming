469b2ef1d638ae3b6c0f7c2d6b3da7a8ee6576cb
Romanandhan <romanandhan@gmail.com>
2021-09-13 17:57:56 +0530

    Try out functions in python by passing different arguments to different functions.

--- Functions_Intro.py
+++ Functions_Intro.py
@@ -3,3 +3,19 @@ def Hello(i,j):
     print(output * 3 )
 
 Hello("Roma", 4)
+
+def square(y):
+    y = y*y
+    return(y)
+x = 10;
+result = square(x)
+print(" The square of ", x, "is", result)
+
+def total(lst):
+    total = 0
+    for item in lst:
+        total = item + total
+    print(total)
+
+lst = [1,2,3,4,5]
+total(lst)
